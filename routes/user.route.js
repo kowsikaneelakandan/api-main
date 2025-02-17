6803614329f329d1e6bd89684ac90a9779df3c54
kowsikaneelakandan <kowsikaneelakandan@gmail.com>
2025-02-17 14:45:31 +0000

    check

--- routes/user.route.js
+++ routes/user.route.js
@@ -9,9 +9,9 @@ import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post("/update/:id", verifyToken, updateUser); //Update User
-router.delete("/delete/:id", verifyToken, deleteUser); //Delete User
-router.get("/listings/:id", verifyToken, getUserListings); //User Listing
-router.get("/:id", verifyToken, getUser); //Get User
+router.post("/update/:id", updateUser); //Update User
+router.delete("/delete/:id", deleteUser); //Delete User
+router.get("/listings/:id",  getUserListings); //User Listing
+router.get("/:id",  getUser); //Get User
 
 export default router;
--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -7,7 +7,7 @@ console.log("token_usecase",token)
 
   if (!token) return next(errorHandler(401, "Unauthorized"));
 
-  jwt.verify(token, process.env.JWT_SECRET || 'todo', (err, user) => {
+  jwt.verify(token, 'todo', (err, user) => {
     if (err) return next(errorHandler(403, "Forbidden"));
     req.user = user;
     next();
